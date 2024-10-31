88c6824ba12ad6ebd9d177db667dd264e2ae2e6a
Alexander <alexsolomahin377@gmail.com>
2024-10-31 22:22:43 +0200

    remove useCallback, rename Board component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Board from "./TicTacToe";
+import Board from "./components/Board/Board";
 
 function App() {
     return (
         <div className="App">
-                <Board />
+            <Board />
         </div>
     );
 }
